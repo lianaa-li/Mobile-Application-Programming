c74b78e0b269cdb7a123b44a2b9613d6886f2326
lianaa-li <liana1@student.umn.ac.id>
2021-04-29 23:26:59 +0700

    add week 12

---
+++ Week12_27963/app/src/main/java/id/ac/umn/week12_27963/FirstActivity.java
@@ -0,0 +1,32 @@
+package id.ac.umn.week12_27963;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FirstActivity extends AppCompatActivity {
+    private TextView tvDaftarSensor;
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        tvDaftarSensor = findViewById(R.id.daftarSensor);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> daftarSensor = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder sensorText = new StringBuilder();
+        for(Sensor currentSensor : daftarSensor) {
+            sensorText.append(currentSensor.getName()).append(
+                    System.getProperty("line.separator")
+            );
+        }
+        tvDaftarSensor.setText(sensorText);
+    }
+}
---
+++ Week12_27963/app/src/main/java/id/ac/umn/week12_27963/MainActivity.java
@@ -0,0 +1,35 @@
+package id.ac.umn.week12_27963;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aktivitasSatu = new Intent(MainActivity.this, FirstActivity.class);
+                startActivityForResult(aktivitasSatu, 1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aktivitasDua = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(aktivitasDua, 1);
+            }
+        });
+    }
+}
